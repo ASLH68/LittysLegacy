a6027b25e21835ce8c42c0313e4b815451506541
Andrea Swihart <andreaswihartdecoster@gmail.com>
2023-02-25 02:11:49 -0600

    Minigame1
    Minigame + recording

---
+++ Assets/Scripts/Minigame1/Minigame1Controller.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minigame1Controller : MonoBehaviour
+{
+    public static Minigame1Controller main;
+
+    private const int MAXITEMS = 9;
+    private int _foundItems = 0;
+
+    public int MaxItems => MAXITEMS;
+    public int FoundItems => _foundItems;
+
+    private void Awake()
+    {
+        if(main == null)
+        {
+            main = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    /// <summary>
+    /// Increments found items
+    /// </summary>
+    public void IncrementItems()
+    {
+        _foundItems++;
+        Minigame1Canvas.main.UpdateFoundItemsText();
+    }
+}
